15e199cc199407fb9f98fb1281106a3951b6a4c5
Daniela Morales <moralesdanielav19@gmail.com>
2017-04-21 09:59:08 -0600

    Add files via upload

---
+++ Usuarios/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <asp:Label ID="Label1" runat="server" Text="Usuario"></asp:Label>
+        <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
+        <asp:Button ID="btnGuardar" runat="server" Text="Guardar" OnClick="btnGuardar_Click"/>
+        <br />
+        <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+        <br />
+        <asp:BulletedList ID="listaUsuarios" runat="server"></asp:BulletedList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Usuarios/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ArrayList list = new ArrayList();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["usuario"] != null)
+        {
+            Literal1.Text = "Hola " + Convert.ToString(Session["usuario"]);
+        }
+        else
+        {
+            Literal1.Text = "No se ha registrado ningun usuario";
+        }
+        if (Cache["Usuarios"] != null)
+        {
+            //recuperamos la lista de usuarios almacenados en cache
+            ArrayList lista = (ArrayList) Cache["Usuarios"];
+            for (int i = 0; i < lista.Count; i++)
+            {
+                //agregamos los usarios a la lista en pantalla
+                listaUsuarios.Items.Add(lista[i].ToString());
+            }
+        }
+    }
+
+    protected void btnGuardar_Click(object sender, EventArgs e)
+    {
+        if (txtUsuario.Text != null)
+        {
+            //Almacenamos al usuario en una variable de sesion
+            Session["usuario"] = txtUsuario.Text;
+            //verificamos si la cache no esta vacia
+            if (Cache["Usuarios"] != null)
+            {
+                //recuperamos los datos de los usuarios almacenados en cache
+                list = (ArrayList)Cache["Usuarios"];
+            }
+            //agregamos usuarios a la lista
+            list.Add(txtUsuario.Text);
+            //los almacenamos en la cache
+            Cache["Usuarios"] = list;
+            //refres de la pagina
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
